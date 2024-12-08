46883ebee5ee6c88fb794181e1f89503c24dc84d
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-12-08 18:31:32 +0100

    day(1): create parameter variable (not used yet)

--- exercise/kotlin/day01/src/test/kotlin/SantaCommunicatorTest.kt
+++ exercise/kotlin/day01/src/test/kotlin/SantaCommunicatorTest.kt
@@ -16,6 +16,10 @@ class SantaCommunicatorTest : DescribeSpec({
 
     describe("composeMessage") {
         it("should compose correct message") {
+            val message = SantaMessage(reindeerName = "Dasher",
+                currentLocation = "North Pole",
+                numbersOfDaysForComingBack = 5,
+                numberOfDaysBeforeChristmas = numberOfDayBeforeChristmas)
             communicator
                 .composeMessage(
                     reindeerName = "Dasher",

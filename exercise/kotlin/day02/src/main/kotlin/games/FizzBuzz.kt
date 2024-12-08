1efedd7e8884aa07bdf2a89f69d60336788037f0
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-12-08 20:45:16 +0100

    day(2): duplicate object into configurable class

--- exercise/kotlin/day02/src/main/kotlin/games/FizzBuzz.kt
+++ exercise/kotlin/day02/src/main/kotlin/games/FizzBuzz.kt
@@ -3,6 +3,7 @@ package games
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
+import games.FizzBuzz.FizzBuzzRule
 
 const val MIN = 1
 const val MAX = 100
@@ -11,6 +12,40 @@ private const val FIZZ = 3
 private const val BUZZ = 5
 
 class ConfigurableFizzBuzz{
+    fun convert(input: Int, vararg rules: FizzBuzzRule): Option<String> {
+        val identityRule = FizzBuzzRule(divisor = 1, { "$it" })
+
+        val applicableRules: Array<out FizzBuzzRule> = when {
+            rules.isNotEmpty() -> rules
+            else -> standardRules()
+        }
+        return when {
+            isOutOfRange(input) -> None
+            else -> {
+                Some(
+                    convertSafely(
+                        input,
+                        *applicableRules,
+                        identityRule
+                    )
+                )
+            }
+        }
+    }
+
+    private fun standardRules() = arrayOf(
+        FizzBuzzRule(FIZZBUZZ, { "FizzBuzz" }),
+        FizzBuzzRule(BUZZ, { "Buzz" }),
+        FizzBuzzRule(FIZZ, { "Fizz" }),
+    )
+
+    private fun convertSafely(input: Int, vararg rules: FizzBuzzRule): String {
+        return rules.toList()
+            .first { rule -> rule.isValid(input) }
+            .apply(input)
+    }
+
+    private fun isOutOfRange(input: Int) = input < MIN || input > MAX
 
 }
 object FizzBuzz {

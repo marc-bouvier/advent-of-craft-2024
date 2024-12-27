f27890b28f89b54d47bc02955dd876f8a6c926c8
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-12-27 13:29:37 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 2
        test: 0
    test-stats:
        run: 5
        passed: 5
        failed: 0
        skipped: 0
        error: 0
        duration: 140ms

--- exercise/kotlin/day24/src/main/kotlin/usecases/ToyDeliveryUseCase.kt
+++ exercise/kotlin/day24/src/main/kotlin/usecases/ToyDeliveryUseCase.kt
@@ -28,7 +28,7 @@ class ToyDeliveryUseCase(private val r: ToyRepository) {
                 }
                 r
             }
-            .map { }
+            .map {Unit }
     }
 
     private fun reduceStock(toy: Toy): Either<Error, Toy> {
